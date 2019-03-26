06cc92b47d29e4c2f0941ae9d8a13d8d7417dab6
karmagamer <kay.pandya@gmail.com>
2019-03-26 19:29:36 +0530

    new files
    new files for practice

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {Field, reduxForm } from 'redux-form';
+import {connect} from 'react-redux';
+import {createStream} from '../../actions/index';
 
 class StreamCreate extends React.Component {
     renderError({error,touched}){
@@ -23,8 +25,9 @@ class StreamCreate extends React.Component {
         );
     }
 
-    onSubmit(formValues){
+    onSubmit = (formValues) => {
         console.log(formValues);
+        this.props.createStream(formValues);
     }
     render(){
         console.log(this.props)
@@ -57,7 +60,11 @@ const validate = (formValues) => {
     return errors;
 };
 
-export default reduxForm({
+const wrappedForm = reduxForm({
     form: 'StreamCreate',
     validate
 })(StreamCreate);
+
+
+
+export default connect(null, { createStream })(wrappedForm);
--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {fetchStreams} from '../../actions';
 
 
-const StreamList = () => {
-    return <div>StreamList</div>;
-};
+class StreamList extends React.Component{
+    componentDidMount(){
+        this.props.fetchStreams();
+    }
+    renderList(){
+        return this.props.streams.map(stream => {
+            return (
+                <div className="item" key={stream.id} >
+                    <i className="large middle aligned icon camera" />
+                    <div className="content">
+                        {stream.Title}
+                        <div className="description">
+                            {stream.Description}
+                        </div>
+                    </div>
+                </div>
+            );
+        })
+    }
+    render(){
+        return (
+        <div>
+            <h2>Stream List</h2>
+            <div className="ui celled list">{this.renderList()}</div>
+        </div>
+        );
+    }
+}
 
-
-export default StreamList;
+const mapStateToProps = (state) => {
+    return {streams: Object.values(state.streams)}
+}
+export default connect(mapStateToProps, {fetchStreams})(StreamList);
